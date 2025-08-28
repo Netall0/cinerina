b3f71809bceda29f47709dce3c0dfc646b14b30d
Netall0 <113532176+Netall0@users.noreply.github.com>
2025-08-28 23:10:55 +0700

    feat: search feature

--- lib/feature/initialization/model/depend_container.dart
+++ lib/feature/initialization/model/depend_container.dart
@@ -1,10 +1,17 @@
+
+import 'package:cinerina/feature/settings/controller/settings_controller.dart';
 import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 final class DependContainer {
-  DependContainer({required this.sharedPreferences, required this.dio,});
+  DependContainer({
+    required this.sharedPreferences,
+    required this.dio,
+    required this.themeController,
+  });
   final SharedPreferences sharedPreferences;
   final Dio dio;
+  final SettingsController themeController;
 }
 
 final class InheritedResult {
